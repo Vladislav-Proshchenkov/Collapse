7598b9cc7cb07f8cd1ac4fba99e3fe6c81fd3982
Vladislav-Proshchenkov <vladislove.pro@gmail.com>
2025-06-09 21:44:53 +0300

    changed

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import './css/style.css';
+import './script.js';
+
+console.log('Webpack работает!');
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html'
+    })
+  ]
+};
