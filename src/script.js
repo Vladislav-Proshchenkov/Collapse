17f140433492b59c6add32e0f0f8fea9a04a24b2
Vladislav-Proshchenkov <vladislove.pro@gmail.com>
2025-06-09 18:43:51 +0300

    init

---
+++ src/script.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const collapsibles = document.getElementsByClassName('collapsible');
+    
+    for (let i = 0; i < collapsibles.length; i++) {
+        collapsibles[i].addEventListener('click', function() {
+            this.classList.toggle('active');
+            const content = this.nextElementSibling;
+            
+            if (content.style.maxHeight) {
+                content.style.maxHeight = null;
+            } else {
+                content.style.maxHeight = content.scrollHeight + 'px';
+            }
+        });
+        
+        if (i === 0) {
+            collapsibles[i].classList.add('active');
+            const content = collapsibles[i].nextElementSibling;
+            content.style.maxHeight = content.scrollHeight + 'px';
+        }
+    }
+});
